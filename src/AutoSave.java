4629b74a63d14654e6fe7ddb8164e33e0db59bbb
averheim <markus.averheim@gmail.com>
2016-12-20 12:54:31 +0100

    AutoSave created. I like. VERY MUCH. Martina, take the wheels!

--- src/AutoSave.java
+++ src/AutoSave.java
@@ -2,4 +2,22 @@
  * Created by MacsMac on 2016-12-20.
  */
 public class AutoSave {
+
+    private RegistryPersister registryPersister = new RegistryPersister();
+
+    public void autoSave() {
+        Thread autoSaveThread = new Thread(() -> {
+            while (true) {
+                try {
+                    synchronized (this) {
+                        Thread.sleep(5000);
+                        registryPersister.save();
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("Autosave failed");
+                }
+            }
+        });
+        autoSaveThread.start();
+    }
 }
