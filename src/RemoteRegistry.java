81c43bd99ed169e35f0518e2630f044d29ecc30b
TheLaMarty <martinaalevin@gmail.com>
2016-12-20 13:09:17 +0100

    Updatez

---
+++ src/RemoteRegistry.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+/**
+ * Created by martina on 2016-12-20.
+ */
+public class RemoteRegistry {
+
+    ArrayList<Contact> remoteContactList = new ArrayList<Contact>();
+
+
+    public ArrayList<Contact> getContacts() {
+        return remoteContactList;
+    }
+
+    public ArrayList<Contact> search(String term) {
+        ArrayList<Contact> searchResult = new ArrayList<>();
+        for (Contact contact : remoteContactList) {
+            if (contact.getFirstName().startsWith(term) || contact.getLastName().startsWith(term)) {
+                searchResult.add(contact);
+            }
+        }
+        return searchResult;
+    }
+
+    public void add(String id, String firstName, String lastName, String email) {
+        remoteContactList.add(new RemoteContact(id, firstName, lastName, email));
+    }
+
+}
