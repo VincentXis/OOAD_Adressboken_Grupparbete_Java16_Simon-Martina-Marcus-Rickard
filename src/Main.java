fadc392a633073399ad5db5e89c68852eb8d3551
SimonPereZ <perez.lencinas@gmail.com>
2016-12-20 10:47:49 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         int wawa = 3; String hej = "";
         String abo = "Abooo";
+
+        String King = "king";
+
     }
 
 }
