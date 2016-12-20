5c85c7814ffc069ab47c9ec7913b18298721660b
VincentXis <rickard.bernal@gmail.com>
2016-12-20 14:01:06 +0100

    Cli fuck

--- src/application/CommandLineInterface.java
+++ src/application/CommandLineInterface.java
@@ -4,6 +4,12 @@ public class CommandLineInterface implements InputHandler {
 
     CommandInterpreter commandInterpreter = new CommandInterpreter();
 
+    private Console console = new Console();
+
+    public void runCommandLineInterface() {
+        console.registerInputHandler(this);
+    }
+
 
     @Override
     public void handle(CommandLine commandLine) {
