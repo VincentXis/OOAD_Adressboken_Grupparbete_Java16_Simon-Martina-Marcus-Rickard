89b850b73d825fbef36a194ab7f9f56444b58c35
TheLaMarty <martinaalevin@gmail.com>
2016-12-20 13:01:36 +0100

    RemoteContact

--- src/Contact.java
+++ src/Contact.java
@@ -4,7 +4,7 @@ import java.util.UUID;
  * Created by MacsMac on 2016-12-20.
  */
 public interface Contact {
-    UUID getId();
+    String getId();
     String getFirstName();
     String getLastName();
     String getEmail();
