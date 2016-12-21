1a2a53025d25024d31a198e850e211e3f6a82643
averheim <markus.averheim@gmail.com>
2016-12-21 14:27:11 +0100

    Fixade mitt huvud

--- src/application/command/ContactFormatter.java
+++ src/application/command/ContactFormatter.java
@@ -4,7 +4,8 @@ import Register.Contact;
 
 public class ContactFormatter {
     public String format(Contact contact){
-        return String.format("ID: %s\nName: %s, %s\nEmail: %s\n", contact.getId(),
+        return String.format("ID: %s\nName: %s, %s\nEmail: %s\n",
+                contact.getId(),
                 contact.getFirstName().substring(0, 1).toUpperCase() + contact.getFirstName().substring(1).toLowerCase(),
                 contact.getLastName().substring(0,1).toUpperCase() + contact.getLastName().substring(1).toLowerCase(),
                 contact.getEmail().toLowerCase());
