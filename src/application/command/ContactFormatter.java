ab0d5ad42a3ec1f013988cd5129627873c74b7b6
averheim <markus.averheim@gmail.com>
2016-12-21 14:19:55 +0100

    Formatering fixad

--- src/application/command/ContactFormatter.java
+++ src/application/command/ContactFormatter.java
@@ -4,6 +4,9 @@ import Register.Contact;
 
 public class ContactFormatter {
     public String format(Contact contact){
-        return String.format("ID: %s\nName: %s, %s\nEmail: %s\n", contact.getId(), contact.getFirstName(), contact.getLastName(), contact.getEmail());
+        return String.format("ID: %s\nName: %s, %s\nEmail: %s\n", contact.getId(),
+                contact.getFirstName().substring(0, 1).toUpperCase() + contact.getFirstName().substring(1).toLowerCase(),
+                contact.getLastName().substring(0,1).toUpperCase() + contact.getLastName().substring(1).toLowerCase(),
+                contact.getEmail().toLowerCase());
     }
 }
