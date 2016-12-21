fc98e2fb4b198adef4f8edf7b315ea69daac474b
averheim <markus.averheim@gmail.com>
2016-12-21 10:55:43 +0100

    References uppdated

--- src/Register/CatalogueLoader.java
+++ src/Register/CatalogueLoader.java
@@ -1,16 +1,30 @@
 package Register;
 
+import java.util.List;
+
 /**
  * Created by martina on 2016-12-20.
  */
 public class CatalogueLoader {
 
-    CatalogueLoader catalogueLoader = new CatalogueLoader();
+    RemoteRegistry remoteRegistry;
+
+    public CatalogueLoader(RemoteRegistry remoteRegistry){
+        this.remoteRegistry = remoteRegistry;
+    }
 
     public void run(){
-        RemoteCatalogueFactory remoteCatalogueFactory = new RemoteCatalogueFactory(1616);
+        RemoteCatalogueFactory remoteCatalogueFactory = new RemoteCatalogueFactory(1616, this);
         new Thread(() -> {
             remoteCatalogueFactory.create("localhost");
         }).start();
+    }
+
+    public void loadRemoteContacts(List<String> remoteContacts){
+        String[] parsedContactInfo;
+        for (String rawContactData : remoteContacts){
+            parsedContactInfo = rawContactData.split(" ");
+        remoteRegistry.add(parsedContactInfo[0], parsedContactInfo[1], parsedContactInfo[2], parsedContactInfo[3]);
+        }
     }
 }
--- src/Register/RemoteCatalogueFactory.java
+++ src/Register/RemoteCatalogueFactory.java
@@ -1,20 +1,18 @@
 package Register;
 
-import java.util.List;
-
 public class RemoteCatalogueFactory {
 
     private int defaultPort;
+    private CatalogueLoader catalogueLoader;
 
-
-
-    public RemoteCatalogueFactory(int port){
+    public RemoteCatalogueFactory(int port, CatalogueLoader catalogueLoader){
         this.defaultPort = port;
+        this.catalogueLoader = catalogueLoader;
     }
 
     public void create(String host){
         AtomicRemoteCatalogueProxy atomicRemoteCatalogueProxy = new AtomicRemoteCatalogueProxy(host, defaultPort);
-        atomicRemoteCatalogueProxy.getContacts();
+        catalogueLoader.loadRemoteContacts(atomicRemoteCatalogueProxy.getContacts());
     }
 
 }
