a9b26a1c99306f7edc13bc8b51ff96b50320d583
VincentXis <rickard.bernal@gmail.com>
2016-12-21 14:31:19 +0100

    fixed search again

--- src/Register/RemoteRegistry.java
+++ src/Register/RemoteRegistry.java
@@ -14,7 +14,8 @@ public class RemoteRegistry {
     public ArrayList<Contact> search(String term) {
         ArrayList<Contact> searchResult = new ArrayList<>();
         for (Contact contact : remoteContactList) {
-            if (contact.getFirstName().startsWith(term) || contact.getLastName().startsWith(term)) {
+            if (contact.getFirstName().toLowerCase().startsWith(term.toLowerCase()) ||
+                    contact.getLastName().toLowerCase().startsWith(term.toLowerCase())) {
                 searchResult.add(contact);
             }
         }
