04cd086dcdf757785bbd6fc653f4ac1df7f2b9c1
TheLaMarty <martinaalevin@gmail.com>
2016-12-21 10:12:01 +0100

    Tagit bort överflödiga instansvariabler

--- src/Register/CatalogueLoader.java
+++ src/Register/CatalogueLoader.java
@@ -1,11 +1,6 @@
 package Register;
 
-/**
- * Created by martina on 2016-12-20.
- */
 public class CatalogueLoader {
-
-    CatalogueLoader catalogueLoader = new CatalogueLoader();
 
     public void run(){
         RemoteCatalogueFactory remoteCatalogueFactory = new RemoteCatalogueFactory(1616);
