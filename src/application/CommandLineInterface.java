2d6c4d8630993dfb5588e6db42cc734124e7e090
VincentXis <rickard.bernal@gmail.com>
2016-12-21 10:09:35 +0100

    wawa aaaaaaa aaaaa aaaaa

--- src/application/CommandLineInterface.java
+++ src/application/CommandLineInterface.java
@@ -21,6 +21,7 @@ public class CommandLineInterface implements InputHandler {
     }
 
     public void runCommandLineInterface() {
+        autoSave.autoSave();
         console.registerInputHandler(this);
     }
 
