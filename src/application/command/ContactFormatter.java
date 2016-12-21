957b6fdb1157454cbeb5e16439e300ad80d79084
averheim <markus.averheim@gmail.com>
2016-12-21 12:46:43 +0100

    Fixed formatting

--- src/application/command/ContactFormatter.java
+++ src/application/command/ContactFormatter.java
@@ -4,7 +4,7 @@ import Register.Contact;
 
 public class ContactFormatter {
     public String format(Contact contact){
-        return String.format("ID: %s\nName:%s, %s\nEmail%s", contact.getId(), contact.getFirstName(), contact.getLastName(), contact.getEmail());
+        return String.format("ID: %s\nName: %s, %s\nEmail: %s", contact.getId(), contact.getFirstName(), contact.getLastName(), contact.getEmail());
     }
 
 }
