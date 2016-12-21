58462e3cc21f176c653dce66bd0b9b82faab9e5e
averheim <markus.averheim@gmail.com>
2016-12-21 12:16:57 +0100

    Main thread created

---
+++ src/application/Main.java
@@ -0,0 +1,11 @@
+package application;
+
+/**
+ * Created by MacsMac on 2016-12-21.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new Application().start();
+    }
+}
