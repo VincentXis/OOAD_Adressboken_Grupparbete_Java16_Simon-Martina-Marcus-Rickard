20b03d8a86b02ef7bc02e029b6f855662535c501
SimonPereZ <perez.lencinas@gmail.com>
2016-12-22 10:54:30 +0100

    add logger and consolprinter to Auto-save

--- src/Register/fileManagement/AutoSave.java
+++ src/Register/fileManagement/AutoSave.java
@@ -1,11 +1,18 @@
 package Register.fileManagement;
 
+import application.Console;
+import application.ConsolePrinter;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by MacsMac on 2016-12-20.
  */
 public class AutoSave {
-
+    private static final Logger log = Logger.getLogger(AutoSave.class.getName());
     private RegistryPersister registryPersister;
+    private ConsolePrinter consolePrinter = new Console();
 
     public AutoSave(RegistryPersister registryPersister) {
         this.registryPersister = registryPersister;
@@ -20,7 +27,8 @@ public class AutoSave {
                         registryPersister.save();
                     }
                 } catch (InterruptedException e) {
-                    System.out.println("Auto-save failed");
+                    log.log(Level.SEVERE,"Auto-save failed",e);
+                    consolePrinter.print("Auto-save failed");
                 }
             }
         }).start();
